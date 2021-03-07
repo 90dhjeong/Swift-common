c28dfaef23d9420d23be96225769bdf79f1763fe
90dhjeong <90dhjeong@gmail.com>
2021-03-07 14:44:24 +0900

    update DeviceInfo.swift

--- DeviceInfo.swift
+++ DeviceInfo.swift
@@ -22,33 +22,29 @@ class DeviceInfo {
      *******************/
     static func getUUID() -> String {
         //  keyChain 에서 긁어와서 없으면, 새로 발급 받은 후 keychain 에 저장
-        var uuid:String? = KeychainWrapper.standard.string(forKey: kSecAttrAccount as String)
+        let uuidString: String? = KeychainWrapper.standard.string(forKey: kSecAttrAccount as String)
         
-        if uuid != nil {
+        if var uuid = uuidString {
             // 값 존재
-            print("uuid : " + uuid!)
-            print(uuid!.count)
+            print("uuid : " + uuid)
             
             let regx = "^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-Z]{4}-[0-9A-F]{4}-[0-9A-F]{12}"
             let checkUuid = NSPredicate(format:"SELF MATCHES %@", regx)
             if  checkUuid.evaluate(with: uuid) {
                 // 올바른 UUID
-                
             } else {
                 // 잘못된 UUID
                 uuid = UUID.init().uuidString
-                KeychainWrapper.standard.set(uuid!, forKey: kSecAttrAccount as String)
+                KeychainWrapper.standard.set(uuid, forKey: kSecAttrAccount as String)
             }
-            
+            return uuid
         } else {
             // 값 없음
             // 새로 생성해서 저장
-            uuid = UUID.init().uuidString
-            KeychainWrapper.standard.set(uuid!, forKey: kSecAttrAccount as String)
+            let uuid = UUID.init().uuidString
+            KeychainWrapper.standard.set(uuid, forKey: kSecAttrAccount as String)
+            return uuid
         }
-        
-//        uuid = "00000000-483e-fced-8d48-55100033c587"
-        return uuid!
     }
     
     /*******************
